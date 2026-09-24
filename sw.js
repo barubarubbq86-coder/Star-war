f516a850d43fd196ffe548d8051248639bfa5d89
barubarubbq86-coder <barubarubbq86@gmail.com>
2026-09-24 14:23:10 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
 // Cache the public game shell. User art remains only in IndexedDB on this device.
 const PREFIX='starling-siege-shell-'+encodeURIComponent(new URL(self.registration.scope).pathname)+'-';
-const CACHE=PREFIX+'v2';
-const SHELL=['./','./index.html','./game.js'];
+const CACHE=PREFIX+'v3';
+const SHELL=['./','./index.html','./game.js','./manifest.webmanifest',
+  './icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
 });
@@ -15,7 +16,8 @@ self.addEventListener('fetch',event=>{
   const url=new URL(event.request.url),scope=new URL(self.registration.scope);
   if(url.origin!==scope.origin||!url.pathname.startsWith(scope.pathname))return;
   const relative=url.pathname.slice(scope.pathname.length);
-  if(!['','index.html','game.js'].includes(relative))return;
+  if(!['','index.html','game.js','manifest.webmanifest',
+    'icon-192.png','icon-512.png'].includes(relative))return;
   event.respondWith(fetch(event.request).then(response=>{
     if(response.ok){
       const copy=response.clone();
